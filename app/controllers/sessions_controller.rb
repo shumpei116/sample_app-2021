953d716a9c81bf0d945d099f5e3e95908e0d3e17
shumpei116 <>
2021-03-27 07:12:56 +0900

    Finish ch09

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    @user = User.find_by(email: params[:session][:email].downcase)
     
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user       
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      remember_me @user
+      redirect_to @user       
     else
       flash.now[:danger] = "Invalid email/password conbination"
       render 'new'
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,9 +4,25 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # ユーザーのセッションを永続的に記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    # まずはsession[:user_id]情報を確認してsessionにユーザー情報がある場合に@current_userをセットする
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+      
+    # session[:user_id]に情報がない場合、cookies情報を確認してcookiesにユーザー情報がある場合@current_userをセットする
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -14,8 +30,21 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+# チェックボックスがチェックされたらリメンバーする
+  def remember_me(user)
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
   end
 end
